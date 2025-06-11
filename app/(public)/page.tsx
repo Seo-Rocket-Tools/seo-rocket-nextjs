9f870ff450e8c40ce7e5d09bf02949e779662a57
francis150 <francisdelavictoria150@gmail.com>
2025-06-11 13:52:45 +0800

    commented out recent posts/blogs

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -659,7 +659,7 @@ export default function Home() {
       </section>
 
       {/* Recent Posts/Blogs Section */}
-      <section className="px-4 sm:px-6 py-16 sm:py-24 relative z-10">
+      {/* <section className="px-4 sm:px-6 py-16 sm:py-24 relative z-10">
         <div className="max-w-7xl mx-auto">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16 items-end mb-12 sm:mb-16">
             <div>
@@ -865,7 +865,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
 
       {/* Contact Us Section */}
       <section className="px-4 sm:px-6 py-16 sm:py-24 relative z-10">
